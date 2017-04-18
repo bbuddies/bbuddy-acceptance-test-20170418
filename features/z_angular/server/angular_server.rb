ba821c0d5966af8f1ef6abf9c29c0f0bdc4432fd
Hedy Chen <hedychen@kkstream.com.tw>
2017-04-18 17:42:54 +0800

    modify angular server sleep time

--- features/z_angular/server/angular_server.rb
+++ features/z_angular/server/angular_server.rb
@@ -3,8 +3,8 @@ require 'optparse'
 def start_server
   `RAILS_ENV=test rails s -d -p 4000`
   angular_folder = ENV['ANGULAR'] ? ENV['ANGULAR'] : '../bbuddy-angular'
-  @gulp_pid = IO.popen("gulp --cwd #{angular_folder} --production &").pid
-  sleep 10
+  @gulp_pid = IO.popen("gulp server --cwd #{angular_folder} --production &").pid
+  sleep 15
 end
 
 def stop_server
