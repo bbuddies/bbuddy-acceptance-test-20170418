cbe1f8b7de2e895e6305fb8807d92eec7c076ba2
Louis Liao <louisliao@kkstream.com.tw>
2017-04-19 10:48:15 +0800

    add update license data and verify error handling scenario

--- features/licenses.feature
+++ features/licenses.feature
@@ -5,3 +5,14 @@ Feature: Licenses
     Then I should see the license
     | month | amount |
     | 2017-04 | 200 |
+
+  Scenario: update licenses data
+    Given I see the data in db
+    When I update the data month to "2017-04" and amount "2000"
+    Then I should see data as below
+      | month | amount |
+      | 2017-04| 2000 |
+
+  Scenario: verify error handling
+    When I input "0"
+    Then I should see error message
--- features/step_definitions/licenses_steps.rb
+++ features/step_definitions/licenses_steps.rb
@@ -14,3 +14,23 @@ Then(/^I should see the license$/) do |table|
   expect(actual.month).to eq(table.hashes[0][:month])
   expect(actual.amount.to_s).to eq(table.hashes[0][:amount])
 end
+
+
+Given(/^I see the data in db$/) do
+
+end
+
+When(/^I update the data month to "([^"]*)" and amount "([^"]*)"$/) do |arg1, arg2|
+
+end
+
+Then(/^I should see data as below$/) do |table|
+end
+
+When(/^I input "([^"]*)"$/) do |arg1|
+
+end
+
+Then(/^I should see error message$/) do
+
+end
